f449ac74e2538b60cd8ca72188c81017ec355cde
Igor <fatalhope@gmail.com>
2021-03-14 11:10:06 +0300

    Added mul and div
    Mul and div operations and their operators processing

--- march_14.py
+++ march_14.py
@@ -17,6 +17,13 @@ def sum_nums(a, b):
 def sub_nums(a, b):
     return a - b
 
+def mul_nums(a, b):
+    return a * b
+
+def div_nums(a, b):
+    return a / b
+
+
 num1 = int(input('ВВедите первое число: '))
 num2 = int(input('Введите второе число: '))
 operator = input('Введите операцию: ')
@@ -25,3 +32,7 @@ if operator == "+":
     print(sum_nums(num1, num2))
 elif operator == "-":
     print(sum_nums(num1, num2))
+elif operator == "*":
+    print(mul_nums(num1, num2))
+elif operator == "/":
+    print(div_nums(num1, num2))

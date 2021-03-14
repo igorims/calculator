41cdfec80dde0fcb2daba21586dceb5dae0133c8
Igor <fatalhope@gmail.com>
2021-03-14 10:18:34 +0300

    Initial commit

---
+++ march_14.py
@@ -0,0 +1,13 @@
+"""MARCH 14, 2021"""
+
+# def smth(a, b:str, c):
+#     """
+#     Description
+#     :param a: a really cool 1st element
+#     :param b: table name
+#     :param c: year of mankind creation
+#     :return: None
+#     """
+#
+# smth()
+
